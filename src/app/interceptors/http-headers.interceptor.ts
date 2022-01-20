9484044fce04c151b286cf9c0af3da388ff23fbc
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2022-01-20 13:59:17 +0200

    last

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() {}
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        req = req.clone({
+            setParams: {
+                key: "d52b968ef2f24c79850117e262e24215"
+            }
+        })
+        return next.handle(req);
+    }
+}
